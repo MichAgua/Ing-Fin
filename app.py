e4e5212d6c1417f4afed933fe764f5646543456b
MichAgua <0240640@up.edu.mx>
2025-04-08 15:03:19 -0600

    Auto update

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import yfinance as yf
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
 
-prompt = st.text_area("escribe tu")
-st.text("Hola Mundo 2")
+st.set_page_config(page_title="The Worlds Foremost and Most Advanced Analyst",layout="wide")
+st.title('The Financial Analyst')
