727ee2f188b21dc35130e3bf16d2ef774e709b0a
MichAgua <0240640@up.edu.mx>
2025-04-08 15:35:00 -0600

    Auto update

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ import pandas as pd
 
 st.set_page_config(page_title="The Worlds Foremost and Most Advanced Analyst",layout="wide")
 st.title('The Financial Analyst')
+
+symbol = st.text_input('Ingrese el ticker de la emisora (por ejemplo, AAPL, NVDA)', 'AAPL')
+
+def get_company_info(ticker)
+    try:
+        info = ticker.info
+        return{
+            'Nombre'
+info.get('shortName', 'Falta de información'),
+        'País': info.get('country', 'Falta de información')
+        'Sector': info.get('sector', 'Falta de información')
+        'Industria': info.get('industria', 'Falta de información')
+        }
