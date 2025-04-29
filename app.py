2e33b04f66e3951db69298ec5854cfab7474e59f
MichAgua <0240640@up.edu.mx>
2025-04-29 15:35:08 -0600

    initial commit

--- app.py
+++ app.py
@@ -74,7 +74,6 @@ symbol = st.text_input('Ingrese el ticker de la emisora')
 def get_company_info(ticker):
     try:
         info = ticker.info
-
         if not isinstance(info, dict) or "shortName" not in info: 
             return None
         
@@ -91,29 +90,12 @@ def get_company_info(ticker):
         'Dividend Yield': info.get('dividendYield', 'Falta de información'),
         'Dividendo por Acción': info.get('dividendRate', 'Falta de información'),
         }
-    
     except Exception as e:
         st.error(f'Error al obtener la información de la emisora: {e}')
         return {}
 
 if symbol:
-    try:
-        ticker = yf.Ticker(symbol)
-        info = get_company_info(ticker)
-        if not info:
-            raise ValueError("No se pudo obtener la información del ticker.")
-        hist = ticker.history(period="5y")
-        if hist.empty:
-            raise ValueError("No se encontraron datos históricos para este ticker.")
-    except Exception as e:
-        st.error(f"Error al obtener la información de la emisora: {e}")
-        st.stop()
-hist = ticker.history(period="5y")
-if hist.empty:
-    st.error("No se encontraron datos históricos para este ticker.")
-    st.stop()
-
-if symbol == 'HACK':
+    if symbol == 'HACK':
     st.markdown("<h1 style='color: lime;'>Sistema Infiltrado</h1>", unsafe_allow_html=True)
     st.markdown("""
         <style>
@@ -127,6 +109,21 @@ if symbol == 'HACK':
     """, unsafe_allow_html=True)
     st.balloons()
     st.stop()
+
+    try:
+        ticker = yf.Ticker(symbol)
+        info = get_company_info(ticker)
+
+        if not info:
+            raise ValueError("No se pudo obtener la información del ticker.")
+        
+        hist = ticker.history(period="5y")
+        if hist.empty:
+            raise ValueError("No se encontraron datos históricos para este ticker.")
+        
+    except Exception as e:
+        st.error(f"Error al obtener la información de la emisora: {e}")
+        st.stop()
 
 st.markdown("""
     <style>
