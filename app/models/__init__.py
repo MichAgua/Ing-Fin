0274f7ab9b7a2d3b078678573758751f55d3a9a6
MichAgua <0240640@up.edu.mx>
2025-06-05 15:12:17 -0600

    initial commit

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/models/__init__.py
-# Solo importa clases si es necesario
-# Por ejemplo:
-# from .pedido import Pedido
-from .user import User
-from .pedido import Pedido
-from .bitacora import Bitacora
-from .cotizacion import Cotizacion
