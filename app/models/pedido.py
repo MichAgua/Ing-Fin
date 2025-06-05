855bd7497c86e83d4a2d65af7153a2c36bec4967
MichAgua <0240640@up.edu.mx>
2025-06-05 13:53:11 -0600

    initial commit

--- app/models/pedido.py
+++ app/models/pedido.py
@@ -1,21 +1,25 @@
-from typing import Optional
 from sqlmodel import SQLModel, Field
-from datetime import datetime, date
+from typing import Optional
+from datetime import datetime
 
 class Pedido(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     cliente: str
-    direccion: Optional[str] = None
-    es_muestra: Optional[bool] = False
-    prenda: Optional[str] = None
-    tipo_tela: Optional[str] = None
-    color: Optional[str] = None
-    talla: Optional[str] = None
-    cantidad: Optional[int] = 1
-    costo_estimado: Optional[float] = 0.0
-    explosion_materiales: Optional[str] = None
-    fecha_entrega: Optional[date] = None
-    usuario_id: Optional[int] = Field(default=None, foreign_key="users.id")
+    direccion: str
+    es_muestra: bool
+    prenda: str
+    tipo_tela: str
+    color: str
+    talla: str
+    cantidad: int
+    costo_estimado: float
+    explosion_materiales: str
+    fecha_entrega: datetime
+    usuario_id: int
     fecha: datetime
     status: str = "pendiente"
+    boton: Optional[str] = None
+    cierre: Optional[str] = None
+    estampado: Optional[str] = None
+    bordado: Optional[str] = None
    
