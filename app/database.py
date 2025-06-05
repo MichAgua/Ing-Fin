b7ea5153b8ab52d3e3e1357704d02825538c14c2
MichAgua <0240640@up.edu.mx>
2025-06-05 13:24:52 -0600

    initial commit

--- app/database.py
+++ app/database.py
@@ -9,11 +9,3 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
-
-with Session(engine) as session:
-    try:
-        st.write("🔍 Consultando pedidos...")
-        pedidos = session.exec(select(Pedido)).all()
-        st.success("Pedidos cargados correctamente.")
-    except Exception as e:
-        st.error(f"⚠️ Error al cargar pedidos: {e}")
